f443e95b41e3736085af2f26f200c2cefdd39fc1
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-07-11 22:01:21 -0600

    Added recursion limit

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,17 @@
 
 #include <iostream>
 
-constexpr auto ray_color(const ray& r, const hittable& world) -> color
+constexpr auto ray_color(const ray& r, const hittable& world, const int depth)
+    -> color
 {
+    if (depth <= 0)
+        return color(0, 0, 0);
+
     auto rec = hit_record();
     if (world.hit(r, 0, infinity, rec))
     {
         auto target = rec.p + rec.normal + random_in_unit_sphere();
-        return 0.5 * ray_color(ray(rec.p, target - rec.p), world);
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
     }
     const auto unit_direction = unit_vector(r.direction());
     const auto t = 0.5 * (unit_direction.y() + 1.0);
@@ -28,6 +32,7 @@ int main()
     constexpr auto image_width = 384;
     constexpr auto image_height = static_cast<int>(image_width / aspect_ratio);
     constexpr auto samples_per_pixel = 100;
+    constexpr auto max_depth = 50;
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
@@ -48,7 +53,7 @@ int main()
                 auto u = (i + random_double()) / (image_width + 1);
                 auto v = (j + random_double()) / (image_height + 1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }

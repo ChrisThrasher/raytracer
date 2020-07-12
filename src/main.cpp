82d66ed46905d00c63a54560c3dddeb1f098f32e
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-07-11 18:20:48 -0600

    Added progress indicator

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ int main()
 
     for (int j = image_height - 1; j >= 0; --j)
     {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i)
         {
             const auto r = double(i) / (image_width - 1);

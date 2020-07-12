46833fd8ba46d3266ffd24f0294afe25c756659b
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-07-12 14:40:43 -0600

    Turned 2 spheres into glass

--- src/main.cpp
+++ src/main.cpp
@@ -42,13 +42,13 @@ int main()
 
     auto world = hittable_list();
     world.add(std::make_shared<sphere>(
-        point3(0, 0, -1), 0.5, std::make_shared<lambertian>(color(0.7, 0.3, 0.3))));
+        point3(0, 0, -1), 0.5, std::make_shared<dielectric>(1.5)));
     world.add(std::make_shared<sphere>(
         point3(0, -100.5, -1), 100, std::make_shared<lambertian>(color(0.8, 0.8, 0.0))));
     world.add(std::make_shared<sphere>(
         point3(1, 0, -1), 0.5, std::make_shared<metal>(color(0.8, 0.6, 0.2), 0.3)));
     world.add(std::make_shared<sphere>(
-        point3(-1, 0, -1), 0.5, std::make_shared<metal>(color(0.8, 0.8, 0.8), 1.0)));
+        point3(-1, 0, -1), 0.5, std::make_shared<dielectric>(1.5)));
 
     constexpr auto cam = camera();
 

4d98d2685352ba04d969bb9bfd2d9ea0098b01a8
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-07-11 22:28:00 -0600

    Added lambertian class

--- src/material.h
+++ src/material.h
@@ -8,3 +8,22 @@ public:
                          color& attenuation,
                          ray& scattered) const = 0;
 }
+
+class lambertian : public material
+{
+    color albedo{};
+
+public:
+    lambertian(const color& a) : albedo(a) {}
+
+    virtual bool scatter(const ray& r_in,
+                         const hit_record& rec,
+                         color& attenuation,
+                         ray& scattered) const
+    {
+            auto scatter_direction = rec.normal + random_unit_vector();
+            scattered = ray(rec.p, scatter_direction);
+            attenuation = albedo;
+            return true;
+    }
+};

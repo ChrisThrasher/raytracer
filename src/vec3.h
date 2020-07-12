f01f70ee60bbb6da63955f42ec375b59101db07e
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-07-11 21:44:07 -0600

    Increased constexpr usage

--- src/vec3.h
+++ src/vec3.h
@@ -9,20 +9,20 @@ class vec3
     std::array<double, 3> e{};
 
 public:
-    vec3() = default;
-    vec3(const double e1, const double e2, const double e3)
+    constexpr vec3() = default;
+    constexpr vec3(const double e1, const double e2, const double e3)
         : e{e1, e2, e3}
     {}
 
-    auto x() const { return e[0]; }
-    auto y() const { return e[1]; }
-    auto z() const { return e[2]; }
+    constexpr auto x() const { return e[0]; }
+    constexpr auto y() const { return e[1]; }
+    constexpr auto z() const { return e[2]; }
 
-    auto operator~() const { return vec3(-e[0], -e[1], -e[2]); }
-    auto operator[](size_t i) const { return e.at(i); }
-    auto& operator[](size_t i) { return e.at(i); }
+    constexpr auto operator~() const { return vec3(-e[0], -e[1], -e[2]); }
+    constexpr auto operator[](size_t i) const { return e.at(i); }
+    constexpr auto& operator[](size_t i) { return e.at(i); }
 
-    auto& operator+=(const vec3& v)
+    constexpr auto& operator+=(const vec3& v)
     {
         e[0] += v.e[0];
         e[1] += v.e[1];
@@ -30,7 +30,7 @@ public:
         return *this;
     }
 
-    auto& operator*=(const double t)
+    constexpr auto& operator*=(const double t)
     {
         e[0] *= t;
         e[1] *= t;
@@ -38,12 +38,12 @@ public:
         return *this;
     }
 
-    auto& operator/=(const double t)
+    constexpr auto& operator/=(const double t)
     {
         return *this *= 1/t;
     }
 
-    auto length2() const
+    constexpr auto length2() const
     {
         return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
     }
@@ -62,49 +62,49 @@ inline auto& operator<<(std::ostream& out, const vec3& v)
     return out << v.x() << ' ' << v.y() << ' ' << v.z();
 }
 
-inline auto operator+(const vec3& lhs, const vec3& rhs)
+constexpr inline auto operator+(const vec3& lhs, const vec3& rhs)
 {
     return vec3(lhs.x() + rhs.x(), lhs.y() + rhs.y(), lhs.z() + rhs.z());
 }
 
-inline auto operator-(const vec3& lhs, const vec3& rhs)
+constexpr inline auto operator-(const vec3& lhs, const vec3& rhs)
 {
     return vec3(lhs.x() - rhs.x(), lhs.y() - rhs.y(), lhs.z() - rhs.z());
 }
 
-inline auto operator-(const vec3& v)
+constexpr inline auto operator-(const vec3& v)
 {
     return vec3(-v.x(), -v.y(), -v.z());
 }
 
-inline auto operator*(const vec3& lhs, const vec3& rhs)
+constexpr inline auto operator*(const vec3& lhs, const vec3& rhs)
 {
     return vec3(lhs.x() * rhs.x(), lhs.y() * rhs.y(), lhs.z() * rhs.z());
 }
 
-inline auto operator*(const double t, const vec3& v)
+constexpr inline auto operator*(const double t, const vec3& v)
 {
     return vec3(t * v.x(), t * v.y(), t * v.z());
 }
 
-inline auto operator*(const vec3& v, const double t)
+constexpr inline auto operator*(const vec3& v, const double t)
 {
     return t * v;
 }
 
-inline auto operator/(const vec3& v, const double t)
+constexpr inline auto operator/(const vec3& v, const double t)
 {
     return (1/t) * v;
 }
 
-inline auto dot(const vec3& lhs, const vec3& rhs)
+constexpr inline auto dot(const vec3& lhs, const vec3& rhs)
 {
     return lhs.x() * rhs.x()
          + lhs.y() * rhs.y()
          + lhs.z() * rhs.z();
 }
 
-inline auto cross(const vec3& lhs, const vec3& rhs)
+constexpr inline auto cross(const vec3& lhs, const vec3& rhs)
 {
     return vec3(lhs.y() * rhs.z() - lhs.z() * rhs.y(),
                 lhs.z() * rhs.x() - lhs.x() * rhs.z(),

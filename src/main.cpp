e43c6fcc57f3898bf13d40ec51228fdd197dfcb3
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-07-11 18:08:36 -0600

    Created image file

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,26 @@
 
 int main()
 {
-    std::cout << "Starting program\n";
+    const auto image_width = 256;
+    const auto image_height = 256;
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        for (int i = 0; i < image_width; ++i)
+        {
+            const auto r = double(i) / (image_width - 1);
+            const auto g = double(j) / (image_height - 1);
+            const auto b = 0.25;
+
+            const auto ir = static_cast<int>(255.999 * r);
+            const auto ig = static_cast<int>(255.999 * g);
+            const auto ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
     return 0;
 }

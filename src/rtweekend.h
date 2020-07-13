21928a69e3c2652c7a46df5e1ccf013cc3d220a7
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-07-12 21:03:56 -0600

    Applied clang-format fixes

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -6,10 +6,7 @@
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
-inline auto DegreesToRadians(const double degrees)
-{
-    return degrees * pi / 180.0;
-}
+inline auto DegreesToRadians(const double degrees) { return degrees * pi / 180.0; }
 
 inline auto RandomDouble()
 {

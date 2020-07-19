4ccaf8c9f564e29ed6a4930fc58ab86611bf2b4b
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-07-19 14:16:25 -0600

    Added u/v coordinates to pixel struct

--- src/Image.h
+++ src/Image.h
@@ -6,6 +6,16 @@
 template <size_t width, size_t height>
 class Image : public std::array<Row<width>, height>
 {
+public:
+    Image()
+    {
+        size_t row_number = 0;
+        for (auto& row : *this)
+        {
+            row = Row<width>(row_number++);
+        }
+    }
+
     friend std::ostream& operator<<(std::ostream& out, const Image<width, height>& image)
     {
         out << "P3\n" << width << ' ' << height << "\n255\n";
--- src/Row.h
+++ src/Row.h
@@ -3,4 +3,17 @@
 #include "Pixel.h"
 
 template <size_t width>
-using Row = std::array<Pixel, width>;
+class Row : public std::array<Pixel, width>
+{
+public:
+    Row() = default;
+    Row(const size_t row_number)
+    {
+        size_t col_number = 0;
+        for (auto& pixel : *this)
+        {
+            pixel.u = col_number++;
+            pixel.v = row_number;
+        }
+    }
+};

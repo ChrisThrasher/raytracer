157060cd7124eddb62d1c027a3bba4ff09a6e137
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-07-27 23:10:28 -0600

    Added pixel numbering test

--- test/ImageTests.cpp
+++ test/ImageTests.cpp
@@ -10,6 +10,33 @@ TEST(Image, Size)
     EXPECT_EQ(400 * sizeof(Pixel), sizeof(image));
 }
 
+TEST(Image, PixelNumbering)
+{
+    constexpr auto image_height = 3ull;
+    constexpr auto image_width = 3ull;
+    auto image = Image<image_width, image_height>();
+    EXPECT_EQ(0, image.At(0).at(0).u);
+    EXPECT_EQ(0, image.At(0).at(0).v);
+    EXPECT_EQ(1, image.At(0).at(1).u);
+    EXPECT_EQ(0, image.At(0).at(1).v);
+    EXPECT_EQ(2, image.At(0).at(2).u);
+    EXPECT_EQ(0, image.At(0).at(2).v);
+
+    EXPECT_EQ(0, image.At(1).at(0).u);
+    EXPECT_EQ(1, image.At(1).at(0).v);
+    EXPECT_EQ(1, image.At(1).at(1).u);
+    EXPECT_EQ(1, image.At(1).at(1).v);
+    EXPECT_EQ(2, image.At(1).at(2).u);
+    EXPECT_EQ(1, image.At(1).at(2).v);
+
+    EXPECT_EQ(0, image.At(2).at(0).u);
+    EXPECT_EQ(2, image.At(2).at(0).v);
+    EXPECT_EQ(1, image.At(2).at(1).u);
+    EXPECT_EQ(2, image.At(2).at(1).v);
+    EXPECT_EQ(2, image.At(2).at(2).u);
+    EXPECT_EQ(2, image.At(2).at(2).v);
+}
+
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);

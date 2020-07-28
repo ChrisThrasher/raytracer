9b19de058227a001b338907dde061761d136fa5d
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-07-27 23:16:12 -0600

    Added Pixel operator<< tests

--- test/ImageTests.cpp
+++ test/ImageTests.cpp
@@ -37,6 +37,30 @@ TEST(Image, PixelNumbering)
     EXPECT_EQ(2, image.At(2).at(2).v);
 }
 
+TEST(Pixel, OutputStreamOperator)
+{
+    {
+        std::stringstream out;
+        out << Pixel{0, 0, 0, 0, 0};
+        EXPECT_EQ("0 0 0\n", out.str());
+    }
+    {
+        std::stringstream out;
+        out << Pixel{0, 0, 0, 1, 1};
+        EXPECT_EQ("0 0 0\n", out.str());
+    }
+    {
+        std::stringstream out;
+        out << Pixel{1, 2, 3, 0, 0};
+        EXPECT_EQ("1 2 3\n", out.str());
+    }
+    {
+        std::stringstream out;
+        out << Pixel{253, 254, 255, 0, 0};
+        EXPECT_EQ("253 254 255\n", out.str());
+    }
+}
+
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);

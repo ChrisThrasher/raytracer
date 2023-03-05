3207ed5473873e0075e52a48c44a3ab31a082dfb
Chris Thrasher <chrisjthrasher@gmail.com>
2023-03-05 16:24:24 -0700

    Add HittableList

---
+++ src/HittableList.cpp
@@ -0,0 +1,37 @@
+#include "HittableList.hpp"
+
+HittableList::HittableList(const std::shared_ptr<Hittable>& object)
+{
+    add(object);
+}
+
+void HittableList::clear()
+{
+    m_objects.clear();
+}
+
+void HittableList::add(const std::shared_ptr<Hittable>& object)
+{
+    m_objects.push_back(object);
+}
+
+auto HittableList::hit(const Ray& ray, float t_min, float t_max) const -> std::optional<HitRecord>
+{
+    auto hit_record = HitRecord();
+    auto hit_something = false;
+    auto closest_yet = t_max;
+
+    for (const auto& object : m_objects)
+    {
+        if(auto maybe_hit_record = object->hit(ray, t_min, closest_yet))
+        {
+            hit_something = true;
+            closest_yet = maybe_hit_record->t;
+            hit_record = *maybe_hit_record;
+        }
+    }
+
+    if (hit_something)
+        return hit_record;
+    return {};
+}
---
+++ src/HittableList.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Hittable.hpp"
+
+#include <memory>
+#include <vector>
+
+class HittableList : public Hittable
+{
+    std::vector<std::shared_ptr<Hittable>> m_objects;
+
+public:
+    HittableList() = default;
+    HittableList(const std::shared_ptr<Hittable>& object);
+
+    void clear();
+    void add(const std::shared_ptr<Hittable>& object);
+
+    [[nodiscard]] auto hit(const Ray& ray, float t_min, float t_max) const -> std::optional<HitRecord> override;
+};

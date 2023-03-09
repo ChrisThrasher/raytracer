60cf4f9e0025a2d42b977d6d5a06ff39ba5d7891
Chris Thrasher <chrisjthrasher@gmail.com>
2023-03-08 22:49:48 -0700

    Add Camera parameters

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,17 +1,18 @@
 #include "Camera.hpp"
 
-namespace {
-constexpr auto aspect_ratio = 16.f / 9.f;
-constexpr auto viewport_height = 2.f;
-constexpr auto viewport_width = aspect_ratio * viewport_height;
-constexpr auto focal_length = 1.f;
-}
+#include <cmath>
 
-Camera::Camera()
-    : m_horizontal(viewport_width, 0, 0)
-    , m_vertical(0, viewport_height, 0)
-    , m_lower_left_corner(m_origin - m_horizontal / 2.f - m_vertical / 2.f - sf::Vector3f(0, 0, focal_length))
+Camera::Camera(const sf::Angle fov, const float aspect_ratio)
 {
+    constexpr auto focal_length = 1.f;
+
+    const auto half_height = std::tan(fov.asRadians() / 2);
+    const auto viewport_height = 2 * half_height;
+    const auto viewport_width = aspect_ratio * viewport_height;
+
+    m_horizontal = { viewport_width, 0, 0 };
+    m_vertical = { 0, viewport_height, 0 };
+    m_lower_left_corner = { m_origin - m_horizontal / 2.f - m_vertical / 2.f - sf::Vector3f(0, 0, focal_length) };
 }
 
 auto Camera::get_ray(const float u, const float v) -> Ray
--- src/Camera.hpp
+++ src/Camera.hpp
@@ -2,6 +2,8 @@
 
 #include "Ray.hpp"
 
+#include <SFML/System/Angle.hpp>
+
 class Camera {
     sf::Vector3f m_origin;
     sf::Vector3f m_horizontal;
@@ -9,7 +11,7 @@ class Camera {
     sf::Vector3f m_lower_left_corner;
 
 public:
-    Camera();
+    Camera(sf::Angle fov, float aspect_ratio);
 
     auto get_ray(float u, float v) -> Ray;
 };

a9833a58483eb572cf4628344dc7553b7bb504ab
Chris Thrasher <chrisjthrasher@gmail.com>
2023-03-09 00:28:34 -0700

    Print rendering status to console

--- src/Raytracer.cpp
+++ src/Raytracer.cpp
@@ -10,6 +10,7 @@
 
 #include <algorithm>
 #include <array>
+#include <atomic>
 #include <chrono>
 #include <iostream>
 #include <memory>
@@ -118,30 +119,49 @@ int main()
     const auto pixels_allocation = std::make_unique<std::array<std::array<sf::Color, image_width>, image_height>>();
     auto& pixels = *pixels_allocation;
 
-    const auto render_rows = [&pixels, camera, &world](const size_t start, const size_t end) noexcept {
-        for (size_t i = start; i < end; ++i) {
-            for (size_t j = 0; j < image_width; ++j) {
-                auto color = sf::Vector3f();
-                for (size_t sample = 0; sample < samples_per_pixel; ++sample) {
-                    const auto u = (random_float(0, 1) + float(j)) / (image_width - 1);
-                    const auto v = (random_float(0, 1) + float(image_height - i)) / (image_height - 1);
-                    const auto ray = camera.get_ray(u, v);
-                    color += ray_color(ray, world, max_depth);
-                }
+    auto rendered_row_count = std::atomic(0);
 
-                pixels[i][j] = to_color(color, samples_per_pixel);
-            }
-        }
-    };
+    const auto render_rows
+        = [&pixels, camera, &world, &rendered_row_count](const size_t start, const size_t end) noexcept {
+              for (size_t i = start; i < end; ++i) {
+                  for (size_t j = 0; j < image_width; ++j) {
+                      auto color = sf::Vector3f();
+                      for (size_t sample = 0; sample < samples_per_pixel; ++sample) {
+                          const auto u = (random_float(0, 1) + float(j)) / (image_width - 1);
+                          const auto v = (random_float(0, 1) + float(image_height - i)) / (image_height - 1);
+                          const auto ray = camera.get_ray(u, v);
+                          color += ray_color(ray, world, max_depth);
+                      }
+
+                      pixels[i][j] = to_color(color, samples_per_pixel);
+                  }
+
+                  ++rendered_row_count;
+              }
+          };
 
     const auto start_of_rendering = std::chrono::steady_clock::now();
 
+    auto rendering = std::atomic(true);
     auto threads = std::vector<std::thread>(std::thread::hardware_concurrency());
-    const auto rows_per_thread = image_height / threads.size();
+    const auto rows_per_thread = float(image_height) / float(threads.size());
+    auto status_thread = std::thread([&rendered_row_count, &rendering]() {
+        while (rendering) {
+            std::cout << "\rRendered " << rendered_row_count << " of " << image_height << " rows" << std::flush;
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        std::cout << std::endl;
+    });
     for (size_t i = 0; i < threads.size(); ++i)
-        threads[i] = std::thread(render_rows, i * rows_per_thread, (i + 1) * rows_per_thread);
+        threads[i]
+            = std::thread(render_rows, size_t(float(i) * rows_per_thread), size_t((float(i) + 1) * rows_per_thread));
     for (auto& thread : threads)
         thread.join();
+    if (rendered_row_count != image_height)
+        throw std::runtime_error("Rendered row count of " + std::to_string(rendered_row_count) + " should be "
+                                 + std::to_string(image_height));
+    rendering = false;
+    status_thread.join();
 
     const auto rendering_time
         = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_of_rendering);

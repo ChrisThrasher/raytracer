2d2b308265c6890355bf4bebcaa90674924c17b8
Chris Thrasher <chrisjthrasher@gmail.com>
2023-03-11 00:29:50 -0700

    Use more correct seed data array type

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -6,7 +6,7 @@
 auto rng() -> std::mt19937&
 {
     thread_local auto generator = []() {
-        auto seed_data = std::array<int, std::mt19937::state_size>();
+        auto seed_data = std::array<std::random_device::result_type, std::mt19937::state_size>();
         auto random_device = std::random_device();
         std::generate_n(seed_data.data(), seed_data.size(), std::ref(random_device));
         auto sequence = std::seed_seq(seed_data.begin(), seed_data.end());
